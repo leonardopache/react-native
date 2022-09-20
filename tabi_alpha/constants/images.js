2fef212712a46066667252605fd46b34bab62c34
Leonardo Pache <leonardo@pache.eng.br>
2022-09-20 18:10:49 +0200

    update version

--- tabi_alpha/constants/images.js
+++ tabi_alpha/constants/images.js
@@ -1,7 +1,9 @@
 const avatar_1 = require("../assets/images/apeapua_logo.png");
 const avatar_2 = require("../assets/images/capivara_01.png");
+const logo = require("../assets/images/apeapua_logo.png");
 
 export default {
     avatar_1,
-    avatar_2
+    avatar_2,
+    logo,
 }
